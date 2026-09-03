d8825d352e424ae65256ed545e7abd2e0b86bf24
Henna <henna.siirila@email.com>
2026-09-03 19:30:08 +0300

    Moduuli 2 tehtävä 3

---
+++ Python_harjoitukset/Moduuli 2/Tehtävä 3.py
@@ -0,0 +1,7 @@
+kanta = input ("anna suorakulmion kanta")
+korkeus = input ("anna suorakulmion korkeus")
+kanta = float (kanta)
+korkeus = float (korkeus)
+piiri= kanta + kanta + korkeus + korkeus
+pinta_ala = kanta * korkeus
+print ("pinta-ala:", pinta_ala, "piiri:", piiri)
