dc0140d96a75fc234d58bfe4686417720641580c
Henna <henna.siirila@email.com>
2026-09-12 14:04:59 +0300

    Moduuli 3 Tehtävä 3

--- Python_harjoitukset/Moduuli 3/Tehtävä 3.py
+++ Python_harjoitukset/Moduuli 3/Tehtävä 3.py
@@ -1,1 +1,19 @@
-kuha=input("Mikä on kuhan pituus")
+sukupuoli=input("Anna sukupuolesi mies/nainen:")
+hemoglobiini=float(input("Anna hemoglobiiniarvo:"))
+if sukupuoli == "nainen":
+   if hemoglobiini < 117:
+      print("hemoglobiiniarvo on liian alhainen.")
+   elif hemoglobiini <=175:
+       print("hemoglobiiniarvo on normaali.")
+   else:
+        print("hemoglobiiniarvo on korkea.")
+
+if sukupuoli == "mies":
+   if hemoglobiini <134:
+      print ("hemoglobiini on liian alhainen.") 
+   elif hemoglobiini <=195:
+          print ("hemoglobiini on normaali.")
+   else:
+        print ("hemoglobiini on korkea.")
+
+
